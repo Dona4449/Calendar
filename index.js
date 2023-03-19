98713fc3f289e9ca87fdb71f15a29e8cd694b70e
ayushjalan <ayushjalan1203@gmail.com>
2023-03-19 14:18:55 +0530

    Full Calendar App made

--- index.js
+++ index.js
@@ -1,46 +1,68 @@
 const date = new Date();
-
-const day = date.getDate();
-console.log(day);
-
-const month = date.getMonth();
-console.log(month);
-
-const year = date.getFullYear();
-console.log(year);
-
-const monthDays = document.querySelector('.dates');
-
-const lastDay = new Date(date.getFullYear(), date.getMonth()+1 , 0).getDate();
-console.log(lastDay);
-
-const months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December"
-];
-
-document.querySelector(".date h1").innerHTML = months[month];
-
-document.querySelector(".date p").innerHTML = date.toDateString();
-
-let days = "";
-
-for(let i=1 ; i<= lastDay ; i++){
-    if(i === new Date().getDate() && date.getMonth() === new Date().getMonth()){
-        days+= `<div class="currDate">${i}</div>`;
+function showCal(date){
+    
+    const day = date.getDate();
+    
+    const month = date.getMonth();
+    
+    const year = date.getFullYear();
+    
+    const monthDays = document.querySelector('.dates');
+    
+    const lastDay = new Date(date.getFullYear(), date.getMonth()+1 , 0).getDate();
+    
+    const firstDayInd = new Date(date.getFullYear(), date.getMonth(), 1).getDay();
+    
+    const months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
+    ];
+    
+    document.querySelector(".date h1").innerHTML = months[month] + " " + date.getFullYear();
+    
+    document.querySelector(".date p").innerHTML = new Date().toDateString();
+    
+    let days = "";
+    
+    for(let j=firstDayInd ; j>=1 ; j--){
+        days+= `<div class="noBorder"></div>`
     }
-    else{
-        days += `<div>${i}</div>`;
-        monthDays.innerHTML = days;
+    
+    for(let i=1 ; i<= lastDay ; i++){
+        if(i === new Date().getDate() && date.getMonth() === new Date().getMonth()){
+            days+= `<div class="currDate">${i}</div>`;
+        }
+        else{
+            days += `<div>${i}</div>`;
+            monthDays.innerHTML = days;
+        }
     }
 }
+
+
+
+document.getElementById("pM").addEventListener("click", function(){
+    date.setMonth(date.getMonth()-1);
+        showCal(date);
+});
+
+document.getElementById("nM").addEventListener("click", function(){
+    date.setMonth(date.getMonth()+1);
+        showCal(date);
+});
+
+document.getElementById("todayDate").addEventListener("click", function(){
+    showCal(new Date());
+});
+
+showCal(date);
